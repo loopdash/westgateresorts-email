554be760d2029578478e1e514384f4ccff380f74
Gary Bunofsky <gary@garybunofsky.com>
2023-03-24 09:59:36 -0400

    v10

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var fs = require('fs');
 var path = require('path');
-var mailgun = require('mailgun-js')({ apiKey: '', domain: 'mg.loopdash.com' });
-var filepath = path.join(__dirname, 'westgateresorts.html');
+var mailgun = require('mailgun-js')({ apiKey: 'key-aad19c5d91483265bf55f0ed69b0283d', domain: 'mg.loopdash.com' });
+var filepath = path.join(__dirname, 'westgateresorts-v10.html');
 var file = fs.readFileSync(filepath, 'utf8');
 
 var data = {
   from: 'Loopdash <test@loopdash.com>',
   to: 'gary@loopdash.com',
-  cc: ['michael@labelmaker.nyc', 'samantha@labelmaker.nyc'],
+  cc: ['samantha@labelmaker.nyc'],
   subject: 'Westgate Resorts',
   html: file,
 };
